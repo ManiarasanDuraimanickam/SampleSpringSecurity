c2b68162380180c1fa4ffa6dc6cc38527c758560
Maniarasan D <maniarasand@iLinkmultitech.com>
2019-05-15 11:48:57 +0530

    [Mani]:[SampleSpringSecutity]: Initial Commit

---
+++ src/main/java/com/ut/SampleSecurityDemo/SampleSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ut.SampleSecurityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ut/SampleSecurityDemo/SampleSecurityDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ut.SampleSecurityDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSecurityDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
